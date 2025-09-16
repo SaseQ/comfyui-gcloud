54ce3326e0f961ca5d9ce56a9d940e6d6b5ca048
SaseQ <bmarczuk03@gmail.com>
2025-09-16 13:56:53 +0200

    edit install_extensions.sh (fix comfyUI-manager installation)

--- src/install_extensions.sh
+++ src/install_extensions.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # Comfy Manager
-git clone https://github.com/ltdrdata/ComfyUI-Manager.git custom_nodes/ComfyUI-Manager
-pip install -r custom_nodes/ComfyUI-Manager/requirements.txt
+git clone https://github.com/ltdrdata/ComfyUI-Manager custom_nodes/comfyui-manager
